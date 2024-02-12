831285bd6228aa85016a286c136b5a2bec5d9bec
RosVor <ros.vorokh@gmail.com>
2024-02-12 22:16:43 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="">
         <ErrorBoundary>
           <App />
         </ErrorBoundary>
