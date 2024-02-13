28db85e6c866a1871969643eb6da3d97358bb9cb
RosVor <101742140+RosVor@users.noreply.github.com>
2024-02-13 21:35:42 +0000

    Deploying to gh-pages from @ RosVor/goit-react-hw-08-phonebook@3126d2768c2cc4416e8a3d37480412507298279b 🚀

---
+++ static/js/593.f48958e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[593],{4593:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(3329),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"Contacts manager :3"})})}}}]);
+//# sourceMappingURL=593.f48958e2.chunk.js.map
